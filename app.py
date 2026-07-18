ce6908158b5f04e45dcc50e9b6eda10d949837e6
Admin1pablo <flavio.rivgopmx@gmail.com>
2026-07-18 16:19:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def init_db():
 
     conn.commit()
     conn.close()
-    init_db()
+    
 @app.route("/")
 def index():
     conn = get_db()
@@ -202,7 +202,7 @@ def create_raffle():
     conn.close()
     flash("Rifa creada correctamente.")
     return redirect(url_for("admin"))
-
+init_db()
 if __name__ == "__main__":
-    init_db()
+    
     app.run(debug=True)
